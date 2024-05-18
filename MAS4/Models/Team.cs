47bf178f45cb502d1844f05a518574ee9dfa0422
JerzyJancewicz <jancewiczjerzy@gmail.com>
2024-05-18 14:08:44 +0200

    mp4 changes

---
+++ MAS4/Models/Team.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAS4.Models
+{
+    public class Team
+    {
+        private string _teamName;
+
+        private HashSet<Worker> AssignedWorkers = new HashSet<Worker>();
+        private HashSet<Worker> ManagedWorkers = new HashSet<Worker>();
+
+        public Team(string teamName)
+        {
+            if (teamName == null)
+            {
+                throw new ArgumentNullException();
+            }
+            _teamName = teamName;
+        }
+        public string TeamName
+        {
+            get { return _teamName;}
+            set { _teamName = value ?? throw new ArgumentException("value can not be null"); }
+        }
+    }
+}
